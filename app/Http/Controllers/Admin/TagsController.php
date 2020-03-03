31ea19e589edaadd85560ad7e10ecd9246c1d392
Kirill Genets <kirill.genets2002@mail.ru>
2020-03-03 21:58:18 +0300

    added tags page

---
+++ app/Http/Controllers/Admin/TagsController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Tag;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TagsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tags = Tag::all();
+
+        return view('admin.tags.index', ['tags' => $tags]);
+    }
+
+    public function create() {
+        return view('admin.tags.create');
+    }
+
+    public function edit($id) {
+        $tag = Tag::find($id);
+
+        return view('admin.tags.edit', ['tag' => $tag]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, ['title' => 'required']);
+        Tag::create($request->all());
+
+        return redirect()->route('tags.index');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, ['title' => 'required']);
+        $tag = Tag::find($id);
+        $tag->update($request->all());
+
+        return redirect()->route('tags.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Tag::find($id)->delete();
+
+        return redirect()->route('tags.index');
+    }
+}
